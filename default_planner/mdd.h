5638458f05d2b32795677115a49425a240fd4e45
Jimmy Lin <lpq66632@gmail.com>
2024-12-18 09:53:48 +1100

    copy mdd.h from CBSH2-RTC code base

--- default_planner/mdd.h
+++ default_planner/mdd.h
@@ -6,3 +6,33 @@
 #define LIFELONG_MDD_H
 
 #endif //LIFELONG_MDD_H
+
+
+
+class MDDNode
+{
+public:
+    MDDNode(int currloc, MDDNode* parent)
+    {
+        location = currloc;
+        if (parent == nullptr)
+            level = 0;
+        else
+        {
+            level = parent->level + 1;
+            parents.push_back(parent);
+        }
+    }
+    int location;
+    int level;
+    int cost=0; // minimum cost of path traversing this MDD node
+
+    bool operator==(const MDDNode& node) const
+    {
+        return (this->location == node.location) && (this->level == node.level);
+    }
+
+
+    list<MDDNode*> children;
+    list<MDDNode*> parents;
+};
