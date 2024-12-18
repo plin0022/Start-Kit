246245c9a55c5d52c01312aed7a1aa6daebb21d7
Jimmy Lin <lpq66632@gmail.com>
2024-12-18 16:16:02 +1100

    Update mdd.h

--- default_planner/mdd.h
+++ default_planner/mdd.h
@@ -2,6 +2,8 @@
 // Created by lpq66 on 18/12/2024.
 //
 
+#include <vector>
+
 #ifndef LIFELONG_MDD_H
 #define LIFELONG_MDD_H
 
@@ -27,8 +29,8 @@ public:
     }
 
 
-    list<MDDNode*> children;
-    list<MDDNode*> parents;
+    vector<MDDNode*> children;
+    vector<MDDNode*> parents;
 };
 
 
@@ -42,18 +44,4 @@ public:
 
     bool buildMDD(const ConstraintTable& ct,
                   int num_of_levels, const SingleAgentSolver* solver);
-    // bool buildMDD(const std::vector <std::list< std::pair<int, int> > >& constraints, int numOfLevels,
-    // 	int start_location, const int* moves_offset, const std::vector<int>& my_heuristic, int map_size, int num_col);
-    void printNodes() const;
-    MDDNode* find(int location, int level) const;
-    void deleteNode(MDDNode* node);
-    void clear();
-    // bool isConstrained(int curr_id, int next_id, int next_timestep, const std::vector< std::list< std::pair<int, int> > >& cons) const;
-
-    void increaseBy(const ConstraintTable& ct, int dLevel, SingleAgentSolver* solver);
-    MDDNode* goalAt(int level);
-
-    MDD() = default;
-    MDD(const MDD& cpy);
-    ~MDD();
 };
