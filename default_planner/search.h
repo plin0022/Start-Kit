3f0c771f4a500215088708bb6817a049204c8cca
Jimmy Lin <lpq66632@gmail.com>
2025-01-20 16:38:35 +1100

    Update search.h

--- default_planner/search.h
+++ default_planner/search.h
@@ -15,6 +15,13 @@ namespace DefaultPlanner{
 s_node astar(SharedEnvironment* env, std::vector<Int4>& flow,
     HeuristicTable& ht, Traj& traj,
     MemoryPool& mem, int start, int goal, Neighbors* ns);
+
+
+// constraints table added version
+    s_node astar(SharedEnvironment* env, std::vector<std::vector<std::pair<bool, std::array<bool, 4>>>>& constraint_flow,
+                 std::vector<Int4>& flow,
+                 HeuristicTable& ht, Traj& traj,
+                 MemoryPool& mem, int start, int goal, Neighbors* ns);
 }
 
 #endif
