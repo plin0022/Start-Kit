0655da463ec604986aec5b8fbf76e505d3575244
Jimmy Lin <lpq66632@gmail.com>
2025-03-14 01:15:03 +1100

    a_star backward

--- default_planner/search.cpp
+++ default_planner/search.cpp
@@ -15,8 +15,6 @@ namespace DefaultPlanner {
 
         init_traffic_heuristic(fh, env, goal, start);
 
-        int h = manhattanDistance(start, goal, env);
-
         s_node *root = mem.get_node(goal);
 
         if (start == goal) {
@@ -25,31 +23,21 @@ namespace DefaultPlanner {
             return *root;
         }
 
-        
-        //todo
-        pqueue_min_of open;
-        re_of re;
 
-        open.push(root);
-
-        int diff, d, cost, op_flow, total_cross, all_vertex_flow, vertex_flow, depth, p_diff, p_d;
-        int next_d1, next_d2, next_d1_loc, next_d2_loc;
+        int diff, d, cost, h;
         int temp_op, temp_vertex;
-        double tie_breaker, decay_factor;
 
-        s_node *goal_node = nullptr;
+        s_node *goal_node = root;
+        s_node *start_node;
         int neighbors[4];
-        int next_neighbors[4];
 
 
-        while (open.size() > 0) {
-            s_node *curr = open.pop();
+        while (!fh.open.empty()) {
+            s_node *curr = fh.open.pop();
+            assert(fh.htable[curr->id] == curr->g);
             curr->close();
 
-            if (curr->id == goal) {
-                goal_node = curr;
-                break;
-            }
+
             getNeighborLocs(ns, neighbors, curr->id);
 
             for (int i = 0; i < 4; i++) {
@@ -58,79 +46,49 @@ namespace DefaultPlanner {
                     continue;
                 }
 
-                cost = curr->g + 1;
-
                 assert(next >= 0 && next < env->map.size());
-                depth = curr->depth + 1;
 
-                //moving direction
-                //flow
-                op_flow = 0;
-                all_vertex_flow = 0;
-
-                h = manhattanDistance(next, goal, env);
+                h = manhattanDistance(next, start, env);
 
 
-                diff = next - curr->id;
+                diff = curr->id - next;
                 d = get_d(diff, env);
-                if (curr->parent != nullptr) {
-                    p_diff = curr->id - curr->parent->id;
-                    p_d = get_d(p_diff, env);
-                    if (p_d != d)
-                        tie_breaker = 0.1;
-                    else
-                        tie_breaker = 0;
-                    //tie breaking on prefering moving forward
-                }
 
 
-                temp_op = ((flow[curr->id].d[d] + 1) *
-                           flow[next].d[(d + 2) % 4]);///( ( (flow[curr->id].d[d]+1) + flow[next].d[(d+2)%4]));
+                temp_op = ((flow[next].d[d] + 1) * flow[curr->id].d[(d + 2) % 4]);
 
-//                    temp_op = std::pow(flow[next].d[(d + 2) % 4], (flow[curr->id].d[d] + 1));
 
                 //all vertex flow
                 //the sum of all out going edge flow is the same as the total number of vertex visiting.
                 temp_vertex = 1;
                 for (int j = 0; j < 4; j++) {
-                    temp_vertex += flow[next].d[j];
+                    temp_vertex += flow[curr->id].d[j];
                 }
 
-                op_flow += temp_op;
+                cost = curr->g + 1 + temp_op + (temp_vertex - 1) / 2;
 
-                all_vertex_flow += (temp_vertex - 1) / 2;
-
-                p_diff = 0;
-                if (curr->parent != nullptr) {
-                    p_diff = curr->id - curr->parent->id;
-                }
-
-                op_flow += curr->op_flow; //op_flow is contra flow
-                all_vertex_flow += curr->all_vertex_flow;
-
-                s_node temp_node(next, cost, h, op_flow, depth);
-                temp_node.tie_breaker = tie_breaker;
-                temp_node.set_all_flow(op_flow, all_vertex_flow);
 
                 if (!mem.has_node(next)) {
-                    s_node *next_node = mem.generate_node(next, cost, h, op_flow, depth, all_vertex_flow);
+                    s_node *next_node = mem.generate_node(next, cost, h, 0, 0, 0);
                     next_node->parent = curr;
-                    next_node->tie_breaker = tie_breaker;
-                    open.push(next_node);
-                } else {
+                    fh.open.push(next_node);
+                    fh.htable[next] = cost;
+                }
+                else {
                     s_node *existing = mem.get_node(next);
-                    if (!existing->is_closed()) {
-                        if (re(temp_node, *existing)) {
-                            existing->g = cost;
-                            existing->parent = curr;
-                            existing->depth = depth;
-                            existing->tie_breaker = tie_breaker;
-                            existing->set_all_flow(op_flow, all_vertex_flow);
-                            open.decrease_key(existing);
-                        }
-                    } else {
+                    assert(existing->g == fh.htable[next]);
 
-                        if (re(temp_node, *existing)) {
+                    if (!existing->is_closed()) {
+                        if (cost < existing->g) {
+                            existing->g = cost;
+                            fh.htable[next] = cost;
+                            existing->parent = curr;
+                            fh.open.decrease_key(existing);
+                        }
+                    }
+                    else {
+
+                        if (cost < existing->g) {
                             std::cout << "error in astar: re-expansion" << std::endl;
                             assert(false);
                             exit(1);
@@ -140,6 +98,11 @@ namespace DefaultPlanner {
                 }
             }
 
+            if (curr->id == start) {
+                goal_node = root;
+                start_node = curr;
+                break;
+            }
 
         }
 
@@ -150,179 +113,188 @@ namespace DefaultPlanner {
             exit(1);
         }
 
-        traj.resize(goal_node->depth + 1);
-        s_node *curr = goal_node;
-        for (int i = goal_node->depth; i >= 0; i--) {
-            traj[i] = curr->id;
+        traj.clear();
+        s_node *curr = start_node;
+
+        while (curr->parent != nullptr)
+        {
+            traj.push_back(curr->id);
             curr = curr->parent;
         }
+        traj.push_back(goal_node->id);
+
+
+
+//        // testing
+//        fh.reset();
+//        init_traffic_heuristic(fh, env, goal, start);
 
         return *goal_node;
     }
 
 
 
-    // original
-    s_node astar(SharedEnvironment *env, std::vector<Int4> &flow,
-                 HeuristicTable &ht, Traj &traj,
-                 MemoryPool &mem, int start, int goal, Neighbors *ns) {
-        mem.reset();
-
-        int expanded = 0;
-        int generated = 0;
-        int h;
-
-        if (ht.empty())
-            h = manhattanDistance(start, goal, env);
-        else
-            h = get_heuristic(ht, env, start, ns);
-
-
-
-        s_node *root = mem.generate_node(start, 0, h, 0, 0, 0);
-
-        if (start == goal) {
-            traj.clear();
-            traj.push_back(start);
-            return *root;
-        }
-
-        pqueue_min_of open;
-        re_of re;
-
-        open.push(root);
-
-        int diff, d, cost, op_flow, total_cross, all_vertex_flow, vertex_flow, depth, p_diff, p_d;
-        int next_d1, next_d2, next_d1_loc, next_d2_loc;
-        int temp_op, temp_vertex;
-        double tie_breaker, decay_factor;
-
-        s_node *goal_node = nullptr;
-        int neighbors[4];
-        int next_neighbors[4];
-
-
-        while (open.size() > 0) {
-            s_node *curr = open.pop();
-            curr->close();
-
-            if (curr->id == goal) {
-                goal_node = curr;
-                break;
-            }
-            expanded++;
-            getNeighborLocs(ns, neighbors, curr->id);
-
-            for (int i = 0; i < 4; i++) {
-                int next = neighbors[i];
-                if (next == -1) {
-                    continue;
-                }
-
-                cost = curr->g + 1;
-
-                assert(next >= 0 && next < env->map.size());
-                depth = curr->depth + 1;
-
-                //moving direction
-                //flow
-                op_flow = 0;
-                all_vertex_flow = 0;
-
-                if (ht.empty())
-                    h = manhattanDistance(next, goal, env);
-                else
-                    h = get_heuristic(ht, env, next, ns);
-
-                diff = next - curr->id;
-                d = get_d(diff, env);
-                if (curr->parent != nullptr) {
-                    p_diff = curr->id - curr->parent->id;
-                    p_d = get_d(p_diff, env);
-                    if (p_d != d)
-                        tie_breaker = 0.1;
-                    else
-                        tie_breaker = 0;
-                    //tie breaking on prefering moving forward
-                }
-
-
-                temp_op = ((flow[curr->id].d[d] + 1) *
-                           flow[next].d[(d + 2) % 4]);///( ( (flow[curr->id].d[d]+1) + flow[next].d[(d+2)%4]));
-
-//                    temp_op = std::pow(flow[next].d[(d + 2) % 4], (flow[curr->id].d[d] + 1));
-
-                //all vertex flow
-                //the sum of all out going edge flow is the same as the total number of vertex visiting.
-                temp_vertex = 1;
-                for (int j = 0; j < 4; j++) {
-                    temp_vertex += flow[next].d[j];
-                }
-
-                op_flow += temp_op;
-
-                all_vertex_flow += (temp_vertex - 1) / 2;
-
-                p_diff = 0;
-                if (curr->parent != nullptr) {
-                    p_diff = curr->id - curr->parent->id;
-                }
-
-                op_flow += curr->op_flow; //op_flow is contra flow
-                all_vertex_flow += curr->all_vertex_flow;
-
-                s_node temp_node(next, cost, h, op_flow, depth);
-                temp_node.tie_breaker = tie_breaker;
-                temp_node.set_all_flow(op_flow, all_vertex_flow);
-
-                if (!mem.has_node(next)) {
-                    s_node *next_node = mem.generate_node(next, cost, h, op_flow, depth, all_vertex_flow);
-                    next_node->parent = curr;
-                    next_node->tie_breaker = tie_breaker;
-                    open.push(next_node);
-                    generated++;
-                } else {
-                    s_node *existing = mem.get_node(next);
-                    if (!existing->is_closed()) {
-                        if (re(temp_node, *existing)) {
-                            existing->g = cost;
-                            existing->parent = curr;
-                            existing->depth = depth;
-                            existing->tie_breaker = tie_breaker;
-                            existing->set_all_flow(op_flow, all_vertex_flow);
-                            open.decrease_key(existing);
-                        }
-                    } else {
-
-                        if (re(temp_node, *existing)) {
-                            std::cout << "error in astar: re-expansion" << std::endl;
-                            assert(false);
-                            exit(1);
-                        }
-
-                    }
-                }
-            }
-
-
-        }
-
-
-        if (goal_node == nullptr) {
-            std::cout << "error in astar: no path found " << start << "," << goal << std::endl;
-            assert(false);
-            exit(1);
-        }
-
-        traj.resize(goal_node->depth + 1);
-        s_node *curr = goal_node;
-        for (int i = goal_node->depth; i >= 0; i--) {
-            traj[i] = curr->id;
-            curr = curr->parent;
-        }
-
-        return *goal_node;
-    }
+//    // original
+//    s_node astar(SharedEnvironment *env, std::vector<Int4> &flow,
+//                 HeuristicTable &ht, Traj &traj,
+//                 MemoryPool &mem, int start, int goal, Neighbors *ns) {
+//        mem.reset();
+//
+//        int expanded = 0;
+//        int generated = 0;
+//        int h;
+//
+//        if (ht.empty())
+//            h = manhattanDistance(start, goal, env);
+//        else
+//            h = get_heuristic(ht, env, start, ns);
+//
+//
+//
+//        s_node *root = mem.generate_node(start, 0, h, 0, 0, 0);
+//
+//        if (start == goal) {
+//            traj.clear();
+//            traj.push_back(start);
+//            return *root;
+//        }
+//
+//        pqueue_min_of open;
+//        re_of re;
+//
+//        open.push(root);
+//
+//        int diff, d, cost, op_flow, total_cross, all_vertex_flow, vertex_flow, depth, p_diff, p_d;
+//        int next_d1, next_d2, next_d1_loc, next_d2_loc;
+//        int temp_op, temp_vertex;
+//        double tie_breaker, decay_factor;
+//
+//        s_node *goal_node = nullptr;
+//        int neighbors[4];
+//        int next_neighbors[4];
+//
+//
+//        while (open.size() > 0) {
+//            s_node *curr = open.pop();
+//            curr->close();
+//
+//            if (curr->id == goal) {
+//                goal_node = curr;
+//                break;
+//            }
+//            expanded++;
+//            getNeighborLocs(ns, neighbors, curr->id);
+//
+//            for (int i = 0; i < 4; i++) {
+//                int next = neighbors[i];
+//                if (next == -1) {
+//                    continue;
+//                }
+//
+//                cost = curr->g + 1;
+//
+//                assert(next >= 0 && next < env->map.size());
+//                depth = curr->depth + 1;
+//
+//                //moving direction
+//                //flow
+//                op_flow = 0;
+//                all_vertex_flow = 0;
+//
+//                if (ht.empty())
+//                    h = manhattanDistance(next, goal, env);
+//                else
+//                    h = get_heuristic(ht, env, next, ns);
+//
+//                diff = next - curr->id;
+//                d = get_d(diff, env);
+//                if (curr->parent != nullptr) {
+//                    p_diff = curr->id - curr->parent->id;
+//                    p_d = get_d(p_diff, env);
+//                    if (p_d != d)
+//                        tie_breaker = 0.1;
+//                    else
+//                        tie_breaker = 0;
+//                    //tie breaking on prefering moving forward
+//                }
+//
+//
+//                temp_op = ((flow[curr->id].d[d] + 1) *
+//                           flow[next].d[(d + 2) % 4]);///( ( (flow[curr->id].d[d]+1) + flow[next].d[(d+2)%4]));
+//
+////                    temp_op = std::pow(flow[next].d[(d + 2) % 4], (flow[curr->id].d[d] + 1));
+//
+//                //all vertex flow
+//                //the sum of all out going edge flow is the same as the total number of vertex visiting.
+//                temp_vertex = 1;
+//                for (int j = 0; j < 4; j++) {
+//                    temp_vertex += flow[next].d[j];
+//                }
+//
+//                op_flow += temp_op;
+//
+//                all_vertex_flow += (temp_vertex - 1) / 2;
+//
+//                p_diff = 0;
+//                if (curr->parent != nullptr) {
+//                    p_diff = curr->id - curr->parent->id;
+//                }
+//
+//                op_flow += curr->op_flow; //op_flow is contra flow
+//                all_vertex_flow += curr->all_vertex_flow;
+//
+//                s_node temp_node(next, cost, h, op_flow, depth);
+//                temp_node.tie_breaker = tie_breaker;
+//                temp_node.set_all_flow(op_flow, all_vertex_flow);
+//
+//                if (!mem.has_node(next)) {
+//                    s_node *next_node = mem.generate_node(next, cost, h, op_flow, depth, all_vertex_flow);
+//                    next_node->parent = curr;
+//                    next_node->tie_breaker = tie_breaker;
+//                    open.push(next_node);
+//                    generated++;
+//                } else {
+//                    s_node *existing = mem.get_node(next);
+//                    if (!existing->is_closed()) {
+//                        if (re(temp_node, *existing)) {
+//                            existing->g = cost;
+//                            existing->parent = curr;
+//                            existing->depth = depth;
+//                            existing->tie_breaker = tie_breaker;
+//                            existing->set_all_flow(op_flow, all_vertex_flow);
+//                            open.decrease_key(existing);
+//                        }
+//                    } else {
+//
+//                        if (re(temp_node, *existing)) {
+//                            std::cout << "error in astar: re-expansion" << std::endl;
+//                            assert(false);
+//                            exit(1);
+//                        }
+//
+//                    }
+//                }
+//            }
+//
+//
+//        }
+//
+//
+//        if (goal_node == nullptr) {
+//            std::cout << "error in astar: no path found " << start << "," << goal << std::endl;
+//            assert(false);
+//            exit(1);
+//        }
+//
+//        traj.resize(goal_node->depth + 1);
+//        s_node *curr = goal_node;
+//        for (int i = goal_node->depth; i >= 0; i--) {
+//            traj[i] = curr->id;
+//            curr = curr->parent;
+//        }
+//
+//        return *goal_node;
+//    }
 
 
 }
