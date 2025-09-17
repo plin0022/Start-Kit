b25dc06beaac1489969710f6bc89901e244f9276
Jimmy Lin <lpq66632@gmail.com>
2025-09-18 00:50:07 +1000

    fw version of sampling

--- default_planner/flow.cpp
+++ default_planner/flow.cpp
@@ -137,11 +137,13 @@ void frank_wolfe(TrajLNS& lns,std::unordered_set<int>& updated, TimePoint timeli
             continue;
         }
 
-        remove_mdd_traj(lns, a);
+        // only update sampling agents
+        if (lns.is_sample[a])
+        {
+            remove_mdd_traj(lns, a);
+            update_traj(lns, a);
+        }
 
-        lns.start_locs[a] = lns.env->curr_states.at(a).location;
-
-        update_traj(lns, a);
         
     }
     return;
@@ -180,7 +182,6 @@ void update_traj(TrajLNS& lns, int i){
     int start = lns.env->curr_states[i].location;
     int goal = lns.tasks[i];
 
-    assert(start == lns.start_locs[i]);
 
 
     // mdd and add_mdd_traj
